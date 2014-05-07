4f95e13310d285026e6c1c20a31411373b74b238
FlorianBasso <florian@ideanov.com>
2014-05-07 12:53:05 +0200

    main menu done

---
+++ Assets/Scripts/Menu/NewRound_btn.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewRound_btn : MonoBehaviour {
+
+	public string levelToLoad;
+	public Sprite normalSprite;
+	public Sprite rollOverSprite;
+	public Sprite clickSprite;
+	public AudioClip beep;
+	
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+	
+	void OnMouseEnter()
+	{
+		this.GetComponent<SpriteRenderer>().sprite = rollOverSprite;
+	}
+	
+	
+	void OnMouseExit()
+	{
+		this.GetComponent<SpriteRenderer>().sprite = normalSprite;
+	}
+	
+	IEnumerator OnMouseUp()
+	{
+		//audio.PlayOneShot (beep);
+		yield return new WaitForSeconds(0.35f);
+		Application.LoadLevel (levelToLoad);
+	}
+	
+	void OnMouseDown()
+	{
+		this.GetComponent<SpriteRenderer>().sprite = clickSprite;
+	}
+}
---
+++ Assets/Scripts/Menu/Quit_btn.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Quit_btn : MonoBehaviour {
+
+	public Sprite normalSprite;
+	public Sprite rollOverSprite;
+	public Sprite clickSprite;
+	public AudioClip beep;
+	
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+	
+	void OnMouseEnter()
+	{
+		this.GetComponent<SpriteRenderer>().sprite = rollOverSprite;
+	}
+	
+	
+	void OnMouseExit()
+	{
+		this.GetComponent<SpriteRenderer>().sprite = normalSprite;
+	}
+	
+	IEnumerator OnMouseUp()
+	{
+		//audio.PlayOneShot (beep);
+		yield return new WaitForSeconds(0.35f);
+		Application.Quit();
+	}
+	
+	void OnMouseDown()
+	{
+		this.GetComponent<SpriteRenderer>().sprite = clickSprite;
+	}
+}
